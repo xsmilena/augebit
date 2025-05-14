2a6f292cdf66474016790d043fceddf3845c7766
Milena Amorim <159318019+xsmilena@users.noreply.github.com>
2025-05-14 14:09:16 -0300

    up

--- FUNCIONARIO/inicial-funcionario/script.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelectorAll(".menu-item");
-
-// Marcar ativo se clicar (opcional no SPA)
-items.forEach(item => {
-  item.addEventListener("click", () => {
-    items.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
